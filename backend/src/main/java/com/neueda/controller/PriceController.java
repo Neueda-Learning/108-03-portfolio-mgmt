92c5aebe6f81626850b07fe5a4e564c73e3ff182
Pranav Menon <pranavmenon2019@gmail.com>
2026-08-03 07:07:14 +0000

    Adding the services

--- backend/src/main/java/com/neueda/controller/PriceController.java
+++ backend/src/main/java/com/neueda/controller/PriceController.java
@@ -3,6 +3,7 @@ package com.neueda.controller;
 import com.neueda.dto.PriceResponse;
 import com.neueda.service.PriceService;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -10,7 +11,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/api/price")
 public class PriceController {
     private final PriceService priceService;
-    public PriceController(PriceService priceService) {
+    public PriceController(@PathVariable PriceService priceService) {
         this.priceService = priceService;
     }
     @GetMapping("/{ticker}")
