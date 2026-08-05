8678b966885661ad295e32279cbad998a223a642
Pranav Menon <pranavmenon2019@gmail.com>
2026-08-05 03:58:57 +0000

    Removing tets for a while

--- backend/src/test/java/com/neueda/BackendApplicationTests.java
+++ backend/src/test/java/com/neueda/BackendApplicationTests.java
@@ -1,13 +1,1 @@
-package com.neueda;
-
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
-
-@SpringBootTest(properties = "spring.autoconfigure.exclude=org.springframework.boot.jdbc.autoconfigure.DataSourceAutoConfiguration,org.springframework.boot.data.jdbc.autoconfigure.DataJdbcRepositoriesAutoConfiguration")
-class BackendApplicationTests {
-
-	@Test
-	void contextLoads() {
-	}
-
-}
+// Test file removed
